32597dfe009a01166b64ed2558e6945b4618ea26
Mali Michael <malimccalla@gmail.com>
2016-09-28 13:58:08 +0100

    Fixes issue with importing post index component

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 
 import App from './components/app';
-import PostsIndex from './components/app';
+import PostsIndex from './components/post_index';
 
 const Greeting = () => {
   return <div>Hey there</div>;
